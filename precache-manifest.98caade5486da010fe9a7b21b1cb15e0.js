7ea5d18f5826d2d3db6bd8c62a4e157ffba1bbe6
ananthu23 <anandusurya4@gmail.com>
2020-05-18 16:38:45 +0530

    Updates

---
+++ precache-manifest.98caade5486da010fe9a7b21b1cb15e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1fc0428c51e57a1e17cc38b25664a4e",
+    "url": "/jokerFirstReact/index.html"
+  },
+  {
+    "revision": "ac201a30f8ed11e6cf82",
+    "url": "/jokerFirstReact/static/css/main.9484bbf6.chunk.css"
+  },
+  {
+    "revision": "334018f314f9c75b24f4",
+    "url": "/jokerFirstReact/static/js/2.086772f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jokerFirstReact/static/js/2.086772f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac201a30f8ed11e6cf82",
+    "url": "/jokerFirstReact/static/js/main.d81f8170.chunk.js"
+  },
+  {
+    "revision": "4ba0ecfbc1e5487dbd4c",
+    "url": "/jokerFirstReact/static/js/runtime-main.696dabc6.js"
+  }
+]);
